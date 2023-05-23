bbcd765e023d946ad0d630098fe200ef233b38f2
TomMacard <123547400+TomMacard@users.noreply.github.com>
2023-05-23 12:24:58 +0200

    tom plateau creation tableau cases

--- src/Model/VariablesGlobales.java
+++ src/Model/VariablesGlobales.java
@@ -2,5 +2,6 @@ package Model;
 
 public class VariablesGlobales {
     public static final int X_MAX=13;
-    public static final int Y_MAX=24;
+    public static final int Y_MAX=16;
+    public static final int DECALAGE=0;
 }
